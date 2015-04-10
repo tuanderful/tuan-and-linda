e2d23b2f013ea5fcf49bbae4aa7a4666572704d8
Tuan Huynh <tuanhuynh@pinterest.com>
2015-04-10 02:38:00 -0700

    Get ready for first release

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -552,7 +552,7 @@ if (!Array.prototype.indexOf) {
 
         /* Instagram Script - change Tag to yours and update ClientId
         ==================================================================================== */
-        var instagramTag = 'wedding'; // Add Instagram Tag here
+        var instagramTag = 'TuanAndLinda'; // Add Instagram Tag here
         var instagramClientId = '630b5c9d2cab44e08fb9d014cf00b2b6'; // Add ClientId here
         var $instagramSection = $('#instagram-section');
         var max_tag_id = false;
